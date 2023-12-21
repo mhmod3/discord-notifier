2b118064404b577cf4e1e618cf8adb75b046f1a9
M7mod <121365613+mhmod3@users.noreply.github.com>
2023-12-21 14:38:03 +0300

    discord notifier.js

--- discord notifier.js
+++ discord notifier.js
@@ -7,9 +7,9 @@ function showNotification(title, message) {
   notifier.notify({
     title: title,
     message: message,
-    appID: 'Discord', // اختياري، يتوافق مع نظام ويندوز 10
+    appID: 'Discord', 
     icon: 'https://cdn.discordapp.com/attachments/1061603211020083233/1187353674213761116/download.png?ex=65969447&is=65841f47&hm=e85e180e68bd4e480766b68422826f8aa39891d86c1e4b2843d925682782b605&', // استبدل الرابط برابط الصورة الخاصة بك
-    sound: true, // قم بتغييره إلى الملف الصوتي الخاص بك
+    sound: true, 
   });
 }
 
@@ -18,12 +18,12 @@ client.on('ready', async () => {
 });
 
 client.on('messageCreate', (message) => {
-  // التحقق مما إذا كانت الرسالة في الخاص
+
   if (message.channel.type === 'DM') {
     const senderName = message.author.username;
     const messageContent = message.content;
 
-    // استدعاء الدالة لعرض الإشعار
+
     showNotification(`New Message ${senderName}`, messageContent);
   }
 });
