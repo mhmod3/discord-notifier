8c9e3d272baf1f02ce7663b419557f43b7854d43
M7mod <121365613+mhmod3@users.noreply.github.com>
2023-12-21 14:38:41 +0300

    discord notifier.js

--- discord notifier.js
+++ discord notifier.js
@@ -1,31 +1,23 @@
 const { Client } = require('discord.js-selfbot-v13');
 const notifier = require('node-notifier');
-
 const client = new Client();
-
 function showNotification(title, message) {
   notifier.notify({
     title: title,
     message: message,
     appID: 'Discord', 
-    icon: 'https://cdn.discordapp.com/attachments/1061603211020083233/1187353674213761116/download.png?ex=65969447&is=65841f47&hm=e85e180e68bd4e480766b68422826f8aa39891d86c1e4b2843d925682782b605&', // استبدل الرابط برابط الصورة الخاصة بك
-    sound: true, 
+    icon: 'https://cdn.discordapp.com/attachments/1061603211020083233/1187353674213761116/download.png?ex=65969447&is=65841f47&hm=e85e180e68bd4e480766b68422826f8aa39891d86c1e4b2843d925682782b605&', 
   });
 }
-
 client.on('ready', async () => {
   console.log(`${client.user.username} is ready!`);
 });
-
 client.on('messageCreate', (message) => {
 
   if (message.channel.type === 'DM') {
     const senderName = message.author.username;
     const messageContent = message.content;
-
-
     showNotification(`New Message ${senderName}`, messageContent);
   }
 });
-
 client.login('Token');//A very important note: (discord.js-selfbot-v13) is used if your account is banned. You are responsible for that.
